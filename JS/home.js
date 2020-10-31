6e5f28bf51df8e2a0e85636bae81b7a93de91d8a
Devansh Goswami <devansh51100@gmail.com>
2020-11-01 00:57:43 +0530

    video

--- JS/home.js
+++ JS/home.js
@@ -8,3 +8,11 @@ if (hamburgers.length > 0) {
     }, false);
   });
 }
+
+
+$(function() {
+  $('a[href*=#]').on('click', function(e) {
+    e.preventDefault();
+    $('html, body').animate({ scrollTop: $($(this).attr('href')).offset().top}, 500, 'linear');
+  });
+});
